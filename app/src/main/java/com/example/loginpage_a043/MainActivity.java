8a6003a933f3eb0d8fe1f04c46bd89b88c9dffe4
Khadijazubair <zubairkhadija6@gmail.com>
2022-04-30 23:55:27 +0500

    Commit

--- app/src/main/java/com/example/loginpage_a043/MainActivity.java
+++ app/src/main/java/com/example/loginpage_a043/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.loginpage_a043;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton LoginBtn=(MaterialButton) findViewById(R.id.LoginBtn);
+
+        //admin and admin
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                   Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                   startActivity(intent);
+                }
+                else {
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
